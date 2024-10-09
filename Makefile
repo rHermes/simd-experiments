54010fcc78e10090449c8366071dc24c42e7a7c9
Teodor Spæren <teodor@sparen.no>
2024-10-09 19:12:48 +0200

    Finish up leetcode 921

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXX = g++
-CXXFLAGS = -O3 -Wall -Wextra -std=c++20 -march=native
+CXXFLAGS = -O3 -Wall -Wextra -std=c++20 -march=native -Iinclude
 
 all: out/simd-shuffle out/leetcode-921
 
@@ -7,13 +7,16 @@ clean:
 	rm out/simd-shuffle
 
 run-simd-shuffle: out/simd-shuffle
-	./out/simd-shuffle
+	@ "./$<"
 
 run-leetcode-921: out/leetcode-921
-	./out/leetcode-921
+	@ "./$<"
+
+obj/nanobench.o: src/nanobench.cpp include/nanobench.h
+	$(CXX) $(CXXFLAGS) -c -o "$@" "$<"
 
 out/simd-shuffle: src/simd-shuffle.cpp src/common.hpp
-	$(CXX) $(CXXFLAGS) -o out/simd-shuffle src/simd-shuffle.cpp
+	$(CXX) $(CXXFLAGS) -o "$@" "$<"
 
-out/leetcode-921: src/leetcode-921.cpp src/common.hpp
-	$(CXX) $(CXXFLAGS) -o out/leetcode-921 src/leetcode-921.cpp
+out/leetcode-921: src/leetcode-921.cpp obj/nanobench.o src/common.hpp
+	$(CXX) $(CXXFLAGS) -o "$@" obj/nanobench.o "$<"
--- src/leetcode-921.cpp
+++ src/leetcode-921.cpp
@@ -1,5 +1,7 @@
 #include "common.hpp"
 
+#include <nanobench.h>
+
 #include <algorithm>
 #include <iomanip>
 #include <iostream>
@@ -21,39 +23,64 @@ solveSIMD_SSE4(std::string_view input);
 //
 template<typename Gen>
 void
-generateInputBernoulli(Gen&& gen, std::span<char> output, const std::size_t N, const float p)
+generateInputBernoulli(Gen&& gen, std::span<char> output, const float p)
 {
-  if (output.size() < N)
-    throw std::runtime_error("Output buffer is not big enough for requested input");
-
   std::bernoulli_distribution dist(p);
-  for (std::size_t i = 0; i < N; i++) {
+  for (std::size_t i = 0; i < output.size(); i++) {
     output[i] = dist(gen) ? '(' : ')';
   }
+}
+
+template<typename Gen>
+std::vector<std::string>
+generateBernoulliTestset(Gen&& gen, const std::size_t size, const std::size_t inputLen, const float p)
+{
+  std::vector<std::string> strings(size);
+  for (auto& s : strings) {
+    s.resize(inputLen);
+    generateInputBernoulli(gen, { s.data(), inputLen }, p);
+  }
+
+  return strings;
+}
+
+template<typename Rng>
+void
+runBernoulliTest(Rng&& rng, const std::string& title, const std::size_t size, const std::size_t inputLen, const float p)
+{
+  ankerl::nanobench::Bench b;
+
+  const auto testSet = generateBernoulliTestset(rng, size, inputLen, p);
+
+  b.title(title).warmup(10).relative(true);
+
+  b.run("Normal", [&]() {
+    for (const auto& s : testSet) {
+      const auto r = solveNormal(s);
+      ankerl::nanobench::doNotOptimizeAway(r);
+    }
+  });
+
+  b.run("SSE4", [&]() {
+    for (const auto& s : testSet) {
+      const auto r = solveSIMD_SSE4(s);
+      ankerl::nanobench::doNotOptimizeAway(r);
+    }
+  });
 }
 
 int
 main()
 {
-  // We are going to try to solve leetcode 921.
-  std::array<char, 4096> bufferRaw;
-  std::span<char, 4096> buffer(bufferRaw);
+  ankerl::nanobench::Bench b;
 
-  std::ranlux48 rng(std::random_device{}());
+  /* b.title("Leetcode 921 solving").warmup(10).minEpochIterations(1000).relative(true); */
+  b.title("Leetcode 921 solving").warmup(10).relative(true);
 
-  for (int i = 0; i < 40; i++) {
-    generateInputBernoulli(rng, buffer, 32, 0.5);
-    std::string_view view{ buffer.data(), 32 };
-
-    const auto expected = solveNormal(view);
-    const auto simdCalc = solveSIMD_SSE4(view);
-
-    if (expected != simdCalc) {
-      std::cout << "Expected: " << std::setw(3) << expected << ", got: " << std::setw(3) << simdCalc << " for: " << view
-                << "\n";
-			break;
-    }
-  }
+  ankerl::nanobench::Rng rng(10);
+  runBernoulliTest(rng, "Short 50% strings", 1000, 10, 0.5);
+  runBernoulliTest(rng, "Mid 50% strings", 1000, 100, 0.5);
+  runBernoulliTest(rng, "Long 50% strings", 1000, 1000, 0.5);
 
   return 0;
 }
@@ -67,15 +94,46 @@ solveSIMD_SSE4(std::string_view inputString)
   int ans = 0;
   int balance = 0;
 
-  for (int i = 0; i < N; i += 16) {
-    const auto chunk = _mm_loadu_si128(reinterpret_cast<__m128i const*>(inputString.data() + i));
-    const auto rightBraces = _mm_sub_epi8(chunk, _mm_set1_epi8(')'));
-#ifdef VERBOSE
-		std::cout << "Input string: [" << inputString.substr(i, 16) << "]\n";
-		std::cout << "Chunk:        [" << m128toHex(rightBraces, true) << "]\n";
-#endif
+  const auto ALL_ONE = _mm_set1_epi8(0x01);
+  const auto ALL_SET = _mm_set1_epi8(0xFF);
 
-    // We want
+  int i = 0;
+  for (; i + 16 <= N; i += 16) {
+    const auto chunk = _mm_loadu_si128(reinterpret_cast<__m128i const*>(inputString.data() + i));
+    const auto leftBraces = _mm_sub_epi8(chunk, _mm_set1_epi8(')'));
+    const auto rightBraces = _mm_andnot_si128(leftBraces, ALL_SET);
+    const auto valueChunk = _mm_or_si128(rightBraces, ALL_ONE);
+
+    auto psa = _mm_add_epi8(valueChunk, _mm_bslli_si128(valueChunk, 1));
+    psa = _mm_add_epi8(psa, _mm_bslli_si128(psa, 2));
+    psa = _mm_add_epi8(psa, _mm_bslli_si128(psa, 4));
+    psa = _mm_add_epi8(psa, _mm_bslli_si128(psa, 8));
+
+    // Now if I have more than 16 in balance,  I can skip the next bit,
+    // but I don't, since it's better to just pipeline everything.
+    auto min = _mm_min_epi8(psa, _mm_bslli_si128(psa, 1));
+    min = _mm_min_epi8(min, _mm_bslli_si128(min, 2));
+    min = _mm_min_epi8(min, _mm_bslli_si128(min, 4));
+    min = _mm_min_epi8(min, _mm_bslli_si128(min, 8));
+
+    const std::int8_t finPsa = static_cast<std::int8_t>(_mm_extract_epi8(psa, 15));
+    const std::int8_t minVal = -static_cast<std::int8_t>(_mm_extract_epi8(min, 15));
+
+    const int padding = std::max(0, minVal - balance);
+    balance += finPsa + padding;
+    ans += padding;
+  }
+
+  for (; i < N; i++) {
+    if (inputString[i] == '(') {
+      balance++;
+    } else {
+      balance--;
+      if (balance < 0) {
+        ans++;
+        balance = 0;
+      }
+    }
   }
 
   return ans + balance;
