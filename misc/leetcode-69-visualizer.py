e805f50e3294baf2fe23f1dd1712a5e7bb44dec4
Teodor Spæren <teo@teleplan.no>
2024-10-25 16:03:29 +0200

    Fix on new format

--- misc/leetcode-69-visualizer.py
+++ misc/leetcode-69-visualizer.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 """
-So this is meant to be a simple
+Spyder Editor
+
+This is a temporary script file.
 """
 
 import drawsvg as dw
@@ -11,7 +13,7 @@ HEIGHT = 700
 
 INS_TEXT_SIZE = 10
 
-BLOCK_TEXT_SIZE = 8
+BLOCK_TEXT_SIZE = 9
 BLOCK_WIDTH = 20
 WHOLE_WIDTH = BLOCK_WIDTH*32
 
@@ -42,7 +44,7 @@ def createAVX2(names: list[tuple[str,int]], offset=None):
         
         block.append(dw.Rectangle(0, 0, BLOCK_WIDTH, BLOCK_WIDTH, fill=fill, stroke="black"))
         
-        txt = dw.Text(name, BLOCK_TEXT_SIZE, BLOCK_WIDTH/2, BLOCK_WIDTH/2, center=True)
+        txt = dw.Text(name, BLOCK_TEXT_SIZE, BLOCK_WIDTH*0.50, BLOCK_WIDTH/2, center=True, font_family="monospace")
         if subs is not None:
             txt.append(dw.TSpan("{}".format(subs), style='font-size: 65%; baseline-shift: sub'))
     
@@ -55,38 +57,70 @@ def createAVX2(names: list[tuple[str,int]], offset=None):
         
     return group
 
+LEFT_PADDING = 40
+
 greenRange = list(Color("lightcyan").range_to(Color("steelblue"), 16))
 pinkRange = list(Color("mistyrose").range_to(Color("tomato"), 16))
 AA = [('A', i, c) for (i, c) in zip(range(0,32), greenRange + pinkRange)]
 BB = [('B', i, "pink") for i in range(0,32)]
 
 
+reverseShuffle = [('{}'.format(i % 16), None, "khaki" if i < 16 else "sandybrown") for i in range(32)]
+reverseShuffle.reverse()
+
+
+basePadding = 20
+
+
 # Initial values
 
-d.append(createAVX2(AA, (10, 20 + 0*VERT_SPACE)))
+gShuf1 = dw.Group(transform="translate(0, {})".format(basePadding))
 
-ag1 = dw.Group(transform="translate({}, {})".format(10 + WHOLE_WIDTH/2, 50 + 0*VERT_SPACE))
-ag1.append(dw.Use(arrow, 0, 0, transform="rotate(90)", fill="black", stroke="black"))
-ag1.append(dw.Text("_mm256_shuffle_epi8", INS_TEXT_SIZE, 5, 20, dominant_baseline="middle", text_anchor="left", font_family="monospace"))
+gShuf1.append(dw.Text("c = _mm256_shuffle_epi8(a, b)", INS_TEXT_SIZE, LEFT_PADDING + WHOLE_WIDTH*0.5, -5, dominant_baseline="bottom", text_anchor="middle", font_family="monospace"))
 
-d.append(ag1)
+gShuf1.append(createAVX2(AA, (LEFT_PADDING, 0)))
+gShuf1.append(dw.Text("a", INS_TEXT_SIZE, LEFT_PADDING-5, BLOCK_WIDTH*0.5, text_anchor="end", dominant_baseline="middle", font_family="monospace"))
 
+gShuf1.append(createAVX2(reverseShuffle, (LEFT_PADDING, BLOCK_WIDTH*1.2)))
+gShuf1.append(dw.Text("b", INS_TEXT_SIZE, LEFT_PADDING-5,  BLOCK_WIDTH*1.2 + BLOCK_WIDTH*0.5, text_anchor="end", dominant_baseline="middle", font_family="monospace"))
 
+gShuf1.append(dw.Text("↓", INS_TEXT_SIZE*2, LEFT_PADDING + WHOLE_WIDTH*0.5, BLOCK_WIDTH*2.2, dominant_baseline="hanging", text_anchor="middle", font_family="monospace"))
 
-# Reshuffle so that they match the actual memory order
 AA = AA[:16][::-1] + AA[16:][::-1]
-d.append(createAVX2(AA, (10, 20 + 1*VERT_SPACE)))
 
-ag2 = dw.Group(transform="translate({}, {})".format(10 + WHOLE_WIDTH/2, 50 + 1*VERT_SPACE))
-ag2.append(dw.Use(arrow, 0, 0, transform="rotate(90)", fill="black", stroke="black"))
-ag2.append(dw.Text("_mm256_permute2x128_si256(xs, xs, 0x01)", INS_TEXT_SIZE, 5, 20, dominant_baseline="middle", text_anchor="left", font_family="monospace"))
+gShuf1.append(dw.Text("c", INS_TEXT_SIZE, LEFT_PADDING-5,  BLOCK_WIDTH*3 + BLOCK_WIDTH*0.5, text_anchor="end", dominant_baseline="middle", font_family="monospace"))
+gShuf1.append(createAVX2(AA, (LEFT_PADDING, BLOCK_WIDTH*3)))
 
-d.append(ag2)
 
-# Flip lanes
+
+
+d.append(gShuf1)
+
+basePadding += VERT_SPACE*1.5
+
+
+gShuf2 = dw.Group(transform="translate(0, {})".format(basePadding))
+
+# Now we flip the lanes
+gShuf2.append(dw.Text("d = _mm256_permute2x128_si256(c, c, 0x01)", INS_TEXT_SIZE, LEFT_PADDING + WHOLE_WIDTH*0.5, -5, dominant_baseline="bottom", text_anchor="middle", font_family="monospace"))
+
+
+gShuf2.append(createAVX2(AA, (LEFT_PADDING, 0)))
+gShuf2.append(dw.Text("c", INS_TEXT_SIZE, LEFT_PADDING-5, BLOCK_WIDTH*0.5, text_anchor="end", dominant_baseline="middle", font_family="monospace"))
+
+gShuf2.append(createAVX2(AA, (LEFT_PADDING, BLOCK_WIDTH*1.2)))
+gShuf2.append(dw.Text("c", INS_TEXT_SIZE, LEFT_PADDING-5, BLOCK_WIDTH*1.2 + BLOCK_WIDTH*0.5, text_anchor="end", dominant_baseline="middle", font_family="monospace"))
+
+gShuf2.append(dw.Text("↓", INS_TEXT_SIZE*2, LEFT_PADDING + WHOLE_WIDTH*0.5, BLOCK_WIDTH*2.2, dominant_baseline="hanging", text_anchor="middle", font_family="monospace"))
+
 AA = AA[16:] + AA[:16]
-d.append(createAVX2(AA, (10, 20 + 2*VERT_SPACE)))
+
+gShuf2.append(dw.Text("d", INS_TEXT_SIZE, LEFT_PADDING-5,  BLOCK_WIDTH*3 + BLOCK_WIDTH*0.5, text_anchor="end", dominant_baseline="middle", font_family="monospace"))
+gShuf2.append(createAVX2(AA, (LEFT_PADDING, BLOCK_WIDTH*3)))
 
 
-# OK, let's create a function for creating just the boxes.
+d.append(gShuf2)
+basePadding += VERT_SPACE*1.5
+
+
 d.save_svg("wow.svg")
